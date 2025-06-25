d57b1f6b602451222313968fcf9da16961a13188
idksarah <sarah.p.ngai@gmail.com>
2025-06-25 13:36:50 -0700

    fuck you reem

--- aurora/player.gd
+++ aurora/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -17,7 +19,10 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.play("run")
+		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("idle")
 
 	move_and_slide()
